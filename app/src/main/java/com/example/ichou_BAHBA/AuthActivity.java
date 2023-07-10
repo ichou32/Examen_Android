6c66939815296b9c14ec5d427e600a2dc4d17929
ichou <ichou.bahba@gmail.com>
2023-07-10 02:33:56 +0100

    last changes

--- app/src/main/java/com/example/ichou_BAHBA/AuthActivity.java
+++ app/src/main/java/com/example/ichou_BAHBA/AuthActivity.java
@@ -1,4 +1,68 @@
-package com.example.ichou_BAHBA;
+package com.example.ichou_BAHBA;//package com.example.ichou_BAHBA;
+//import android.content.Intent;
+//import android.content.SharedPreferences;
+//import android.os.Bundle;
+//import android.view.View;
+//import android.widget.Button;
+//import android.widget.EditText;
+//import android.widget.Toast;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//public class AuthActivity extends AppCompatActivity {
+//
+//    private EditText editTextUsername;
+//    private EditText editTextPassword;
+//    private Button buttonLogin;
+//
+//    private SharedPreferences sharedPreferences;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_auth);
+//
+//        editTextUsername = findViewById(R.id.editTextUsername);
+//        editTextPassword = findViewById(R.id.editTextPassword);
+//        buttonLogin = findViewById(R.id.buttonLogin);
+//
+//        sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+//
+//        buttonLogin.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String username = editTextUsername.getText().toString();
+//                String password = editTextPassword.getText().toString();
+//
+//                // Vérifier si les informations de connexion sont valides
+//                if (isValidCredentials(username, password)) {
+//                    saveCredentials(username, password);
+//                    startMainActivity();
+//                } else {
+//                    Toast.makeText(AuthActivity.this, "Invalid credentials", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
+//    }
+//
+//    private boolean isValidCredentials(String username, String password) {
+//        // Vérifier les informations de connexion (par exemple, en comparant avec des valeurs prédéfinies)
+//        return username.equals("admin") && password.equals("password");
+//    }
+//
+//    private void saveCredentials(String username, String password) {
+//        SharedPreferences.Editor editor = sharedPreferences.edit();
+//        editor.putString("username", username);
+//        editor.putString("password", password);
+//        editor.apply();
+//    }
+//
+//    private void startMainActivity() {
+//        Intent intent = new Intent(this, MainActivity.class);
+//        startActivity(intent);
+//        finish();
+//    }
+//}
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
--- app/src/main/java/com/example/ichou_BAHBA/FullMapActivity.java
+++ app/src/main/java/com/example/ichou_BAHBA/FullMapActivity.java
@@ -1,11 +1,6 @@
 package com.example.ichou_BAHBA;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,48 +16,34 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class MainActivity extends AppCompatActivity {
+public class FullMapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private MapView mapView;
     private GoogleMap googleMap;
 
-    private EditText editTextIPAddress;
-    private Button buttonSearch;
+    private String ipAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_full_map);
+
+        ipAddress = getIntent().getStringExtra("ipAddress");
 
         mapView = findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(GoogleMap googleMap) {
-                MainActivity.this.googleMap = googleMap;
-            }
-        });
+        mapView.getMapAsync(this);
+    }
 
-        editTextIPAddress = findViewById(R.id.editTextIPAddress);
-        buttonSearch = findViewById(R.id.buttonSearch);
-
-        buttonSearch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String ipAddress = editTextIPAddress.getText().toString();
-                if (!TextUtils.isEmpty(ipAddress)) {
-                    getLocationDetails(ipAddress);
-                } else {
-                    Toast.makeText(MainActivity.this, "Please enter an IP address", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        getLocationDetails(ipAddress);
     }
 
     private void getLocationDetails(String ipAddress) {
@@ -73,23 +54,18 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
-                            String location = response.getString("loc");
-                            String[] latLng = location.split(",");
+                            String city = response.getString("city");
+                            String region = response.getString("region");
+                            String country = response.getString("country");
+                            String loc = response.getString("loc");
+
+                            String[] latLng = loc.split(",");
                             double latitude = Double.parseDouble(latLng[0]);
                             double longitude = Double.parseDouble(latLng[1]);
 
                             LatLng latLngLocation = new LatLng(latitude, longitude);
-                            googleMap.addMarker(new MarkerOptions().position(latLngLocation).title(ipAddress));
+                            googleMap.addMarker(new MarkerOptions().position(latLngLocation).title(city));
                             googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLngLocation, 10f));
-
-                            // Show the full map activity when clicking on the marker
-                            googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-                                @Override
-                                public boolean onMarkerClick(Marker marker) {
-                                    showFullMapActivity(ipAddress);
-                                    return false;
-                                }
-                            });
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -98,17 +74,11 @@ public class MainActivity extends AppCompatActivity {
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(MainActivity.this, "Error retrieving location", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(FullMapActivity.this, "Error retrieving location", Toast.LENGTH_SHORT).show();
                     }
                 });
 
         queue.add(request);
-    }
-
-    private void showFullMapActivity(String ipAddress) {
-        Intent intent = new Intent(MainActivity.this, FullMapActivity.class);
-        intent.putExtra("ipAddress", ipAddress);
-        startActivity(intent);
     }
 
     @Override
